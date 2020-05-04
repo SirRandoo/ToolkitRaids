b59a8ab8814053a6baaa2d1afe26f4ef9ef438b2
SirRandoo <randomshovel@gmail.com>
2020-05-04 15:57:44 -0500

    Added relevant settings to the mod.

--- ToolkitRaids/Settings.cs
+++ ToolkitRaids/Settings.cs
@@ -1,18 +1,55 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using Verse;
 
 namespace SirRandoo.ToolkitRaids
 {
     public class Settings : ModSettings
     {
+        public static bool MergeRaids;
+        public static int Duration = 60;
 
         public static void Draw(Rect canvas)
         {
+            var listing = new Listing_Standard{maxOneColumn = true};
+
+            listing.Begin(canvas);
+            
+            listing.CheckboxLabeled(
+                "ToolkitRaids.MergeRaids.Label".Translate(),
+                ref MergeRaids,
+                "ToolkitRaids.MergeRaids.Tooltip".Translate()
+            );
+
+            var (timeLabel, timeField) = ToForm(listing.GetRect(Text.LineHeight));
+            var durationBuffer = Duration.ToString();
+            
+            Widgets.Label(timeLabel, "ToolkitRaids.Duration.Label".Translate());
+            Widgets.TextFieldNumeric(timeField, ref Duration, ref durationBuffer, 30, 600);
+
+            if (Mouse.IsOver(timeLabel))
+            {
+                Widgets.DrawHighlightIfMouseover(timeLabel);
+                TooltipHandler.TipRegion(timeLabel, "ToolkitRaids.Duration.Tooltip".Translate());
+            }
+            
+            listing.End();
         }
 
         public override void ExposeData()
         {
-            
+            Scribe_Values.Look(ref MergeRaids, "mergeRaids");
+            Scribe_Values.Look(ref Duration, "duration", 60);
+        }
+
+        public static Tuple<Rect, Rect> ToForm(Rect region)
+        {
+            var left = new Rect(region.x, region.y, region.width * 0.85f - 2f, region.height);
+
+            return new Tuple<Rect, Rect>(
+                left,
+                new Rect(left.x + left.width + 2f, left.y, region.width - left.width - 2f, left.height)
+            );
         }
     }
 }
