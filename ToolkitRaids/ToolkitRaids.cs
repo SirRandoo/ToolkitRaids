41e244a6a0df401fcc37fbd15f4417b000a08a96
SirRandoo <randomshovel@gmail.com>
2020-05-04 18:08:21 -0500

    Added an event handler for TwitchLib's raid event.

--- ToolkitRaids/ToolkitRaids.cs
+++ ToolkitRaids/ToolkitRaids.cs
@@ -1,10 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Collections.Concurrent;
+using TwitchLib.Client.Events;
+using UnityEngine;
 using Verse;
 
 namespace SirRandoo.ToolkitRaids
 {
     public class ToolkitRaids : Mod
     {
+        public static ConcurrentQueue<string> RecentRaids = new ConcurrentQueue<string>();
+        
         public ToolkitRaids(ModContentPack content) : base(content)
         {
             GetSettings<Settings>();
@@ -13,5 +17,18 @@ namespace SirRandoo.ToolkitRaids
         public override void DoSettingsWindowContents(Rect inRect) => Settings.Draw(inRect);
 
         public override string SettingsCategory() => nameof(ToolkitRaids);
+    }
+
+    public static class ToolkitRaidsStatic
+    {
+        static ToolkitRaidsStatic()
+        {
+            ToolkitCore.TwitchWrapper.Client.OnRaidNotification += OnRaidNotification;
+        }
+
+        private static void OnRaidNotification(object sender, OnRaidNotificationArgs args)
+        {
+            ToolkitRaids.RecentRaids.Enqueue(args.RaidNotification.Login);
+        }
     }
 }
